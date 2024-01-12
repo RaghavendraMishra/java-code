69c0b429a6c1e17d4a4bfb411e648bd55a63e537
mveeraragahavan <156316797+mveeraragahavan@users.noreply.github.com>
2024-01-12 06:11:08 +0000

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,9 +1,10 @@
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+		System.out.println("I AM VEERARAGAHAVAN");
 		System.out.println("**************************************");
-		System.out.println("Hello from GITHub");
-		System.out.println("**************************************");
+		System.out.println("The Java file has been executes successfully");
 	}
 
 }
